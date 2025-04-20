d5dcd4be5f57a7f1ff3d76e7f93e87e11f88972c
0BooM <149960425+0BooM@users.noreply.github.com>
2025-04-20 13:23:13 +0200

    Complete Calculator object

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ export function capitalize(string) {
 export function reverseString(string){
     return string.split("").reverse().join("");
 }
+
+export const calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    divide: (a, b) => {
+        if (b === 0) throw new Error("Cannot divide by zero!");
+        return a / b;
+    },
+    multiply: (a, b) => a * b,
+}
