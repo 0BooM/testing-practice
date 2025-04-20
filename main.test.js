e6ec3cef85fcff32a1ee50114d57efad4e07e6dd
0BooM <149960425+0BooM@users.noreply.github.com>
2025-04-20 13:23:03 +0200

    Write tests for Calculator object

--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import {capitalize, reverseString} from "./main";
+import {capitalize, reverseString, calculator} from "./main";
 
 test("Capitalize first character", () => {
   expect(capitalize("")).toBe("");
@@ -12,3 +12,29 @@ test("Reverse a string", () => {
     expect(reverseString("123")).toBe("321");
     expect(reverseString("")).toBe("");
 })
+
+describe('Calculator', () => {
+  test('add', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+    expect(calculator.add(-1, 2)).toBe(1);
+    expect(calculator.add(-2, -3)).toBe(-5);
+  });
+
+  test('subtract', () => {
+    expect(calculator.subtract(1, 2)).toBe(-1);
+    expect(calculator.subtract(-1, 2)).toBe(-3);
+    expect(calculator.subtract(5, 2)).toBe(3);
+  });
+
+  test("divide", () => {
+    expect(calculator.divide(1, 2)).toBeCloseTo(0.5);
+    expect(calculator.divide(6, 3)).toBe(2);
+    expect(() => calculator.divide(10, 0)).toThrow("Cannot divide by zero!");
+  });
+
+  test("multiply", () => {
+    expect(calculator.multiply(3, 4)).toBe(12);
+    expect(calculator.multiply(-2, 5)).toBe(-10);
+    expect(calculator.multiply(0, 5)).toBe(0);
+  });
+});
